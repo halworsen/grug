747c68ab124eb312d8a87590bf596f64e3fdb420
halworsen <markus@halvorsenfamilien.com>
2021-07-13 18:45:54 +0200

    more helper actions

--- helper_actions.go
+++ helper_actions.go
@@ -1,7 +1,10 @@
 package grug
 
 import (
+	"encoding/json"
+	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/aquilax/truncate"
 	"github.com/davecgh/go-spew/spew"
@@ -66,8 +69,73 @@ func init() {
 				}
 
 				truncated := truncate.Truncate(val, truncTo, "...", truncate.PositionEnd)
-				g.Log(logDebug, len(truncated))
 				return truncated, nil
+			},
+		},
+		{
+			// Performs a string of accesses
+			// E.g. FieldA.0.B would access args[0].FieldA[0].B
+			Name: "FieldAccess",
+			Exec: func(g *GrugSession, args ...interface{}) (interface{}, error) {
+				if args[1] == "." {
+					return args[0], nil
+				}
+				indexPlan := strings.Split(atostr(args[1]), ".")
+
+				var value interface{}
+				for n, field := range indexPlan {
+					if n == 0 {
+						value = args[0]
+					}
+
+					idx, err := strconv.Atoi(field)
+					if err == nil {
+						valAsSlice, ok := value.([]interface{})
+						if !ok {
+							return nil, fmt.Errorf("%v is not accessible by index", value)
+						}
+						value = valAsSlice[idx]
+					} else {
+						valAsMap, ok := value.(map[string]interface{})
+						if !ok {
+							return nil, fmt.Errorf("%v is not accessible by name %s", value, field)
+						}
+						value = valAsMap[field]
+					}
+				}
+
+				return value, nil
+			},
+		},
+		{
+			// Converts the input to a JSON string
+			Name: "ToJSON",
+			Exec: func(g *GrugSession, args ...interface{}) (interface{}, error) {
+				json, err := json.MarshalIndent(args[0], "", "  ")
+				if err != nil {
+					return nil, err
+				}
+				return string(json), nil
+			},
+		},
+		{
+			// Concatenates all args as strings
+			Name: "Concatenate",
+			Exec: func(g *GrugSession, args ...interface{}) (interface{}, error) {
+				argsAsStr := make([]string, 0)
+				for _, arg := range args {
+					if argStr, ok := arg.(string); ok {
+						argsAsStr = append(argsAsStr, argStr)
+					}
+				}
+				return strings.Join(argsAsStr, ""), nil
+			},
+		},
+		{
+			// Passes arg[0] back directly
+			Name: "Passthrough",
+			Exec: func(g *GrugSession, args ...interface{}) (interface{}, error) {
+				return args[0], nil
 			},
 		},
 	}...)
